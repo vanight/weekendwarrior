6590b5d110c35746c0397904ad441dfecd86b910
vanight <712220+vanight@users.noreply.github.com>
2021-04-04 22:39:44 -0700

    Add mat; add card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'weekendwarrior';
+  defaultElevation = 2;
+  raisedElevation = 8;
+
+  name = 'Angular';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialElevationDirective } from '../lib/directive/material-elevation.directive';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MaterialElevationDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
