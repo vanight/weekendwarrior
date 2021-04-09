b96f62c88e0b9854bd6aad4b3d537ec7dd6692fe
Chao Yu <chaoyu.bupter@gmail.com>
2021-04-09 12:11:15 -0700

    add more picture;add logo

--- src/app/tools-dashboard/tools-dashboard.component.ts
+++ src/app/tools-dashboard/tools-dashboard.component.ts
@@ -11,12 +11,19 @@ export class ToolsDashboardComponent {
   /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
-
+      let cols = 1
+      if ( matches ) {
+        cols = 2;
+      }
       return [
-        { title: 'Miter Saw', cols: 1, rows: 1, image: "https://images-na.ssl-images-amazon.com/images/I/71TPrh4okNL._AC_SL1500_.jpg", desc: "For small wood working projecsts" },
-        { title: 'Pressure Washer', cols: 1, rows: 1, image: "https://images-na.ssl-images-amazon.com/images/I/91Or5s3f24L._AC_SL1500_.jpg", desc: "Pressure wash your deck"},
-        { title: 'Drill Set', cols: 1, rows: 1, image: "https://images-na.ssl-images-amazon.com/images/I/91WzLZ1qASL._AC_SL1500_.jpg", desc: "Any project would be needed" },
-        { title: 'Edger and Trimmer', cols: 1, rows: 1, image: "https://images-na.ssl-images-amazon.com/images/I/512jaAufjAL._AC_SL1000_.jpg", desc:"Trim your grass, once a year" }
+        { title: 'Pressure Washer', cols: cols, rows: 1, image: "https://images-na.ssl-images-amazon.com/images/I/91Or5s3f24L._AC_SL1500_.jpg", desc: "Pressure wash your deck"},
+        { title: 'Drill Set', cols: cols, rows: 1, image: "https://images-na.ssl-images-amazon.com/images/I/91WzLZ1qASL._AC_SL1500_.jpg", desc: "Any project would be needed" },
+        { title: 'Edger and Trimmer', cols: cols, rows: 1, image: "https://images-na.ssl-images-amazon.com/images/I/512jaAufjAL._AC_SL1000_.jpg", desc:"Trim your grass edge, make your lawn a clean look" },
+        { title: 'Miter Saw', cols: cols, rows: 1, image: "https://images-na.ssl-images-amazon.com/images/I/71TPrh4okNL._AC_SL1500_.jpg", desc: "For small wood working projecsts" },
+        { title: '22-foot telescoping ladder', cols: cols, rows: 1, image: "https://images-na.ssl-images-amazon.com/images/I/51M9J2MlE9S._AC_SL1000_.jpg", desc: "Easily anything on your first floor" },
+        { title: '32-feet extension ladder', cols: cols, rows: 1, image: "https://images-na.ssl-images-amazon.com/images/I/31VJHN3H1JL._AC_.jpg", desc: "Reach your second floor" },
+
+        { title: 'Service', cols: cols, rows: 1, image: "assets/ww-logo.svg", desc: "Weekend Warrior service" },
       ];
     })
   );
