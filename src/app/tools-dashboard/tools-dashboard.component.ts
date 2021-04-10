d868c41ee6899f492d9b131daaf1c86fd10b6f08
Chao Yu <chaoyu.bupter@gmail.com>
2021-04-10 11:25:38 -0700

    Add map and contact us

--- src/app/tools-dashboard/tools-dashboard.component.ts
+++ src/app/tools-dashboard/tools-dashboard.component.ts
@@ -5,27 +5,99 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 @Component({
   selector: 'app-tools-dashboard',
   templateUrl: './tools-dashboard.component.html',
-  styleUrls: ['./tools-dashboard.component.css']
+  styleUrls: ['./tools-dashboard.component.css'],
 })
 export class ToolsDashboardComponent {
   /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
-      let cols = 1
-      if ( matches ) {
+      let cols = 1;
+      if (matches) {
         cols = 2;
       }
       return [
-        { title: 'Pressure Washer', cols: cols, rows: 1, image: "https://images-na.ssl-images-amazon.com/images/I/91Or5s3f24L._AC_SL1500_.jpg", desc: "Pressure wash your deck"},
-        { title: 'Drill Set', cols: cols, rows: 1, image: "https://images-na.ssl-images-amazon.com/images/I/91WzLZ1qASL._AC_SL1500_.jpg", desc: "Any project would be needed" },
-        { title: 'Edger and Trimmer', cols: cols, rows: 1, image: "https://images-na.ssl-images-amazon.com/images/I/512jaAufjAL._AC_SL1000_.jpg", desc:"Trim your grass edge" },
-        { title: 'Nailer Gun', cols: cols, rows: 1, image: "https://images-na.ssl-images-amazon.com/images/I/51gokfO5USL._AC_SL1000_.jpg", desc:"Easy to use for small project" },
-        { title: 'Miter Saw', cols: cols, rows: 1, image: "https://images-na.ssl-images-amazon.com/images/I/71TPrh4okNL._AC_SL1500_.jpg", desc: "For small woodworking projects, with a stand" },
-        { title: 'Table Saw', cols: cols, rows: 1, image: "https://images.thdstatic.com/productImages/3f75b59d-b153-45fd-ad06-052296e477f1/svn/dewalt-portable-table-saws-dw745-64_600.jpg", desc: "For small woodworking projects, with a stand" },
-        { title: '22-foot telescoping ladder', cols: cols, rows: 1, image: "https://images-na.ssl-images-amazon.com/images/I/51M9J2MlE9S._AC_SL1000_.jpg", desc: "A versitle ladder for anything" },
-        { title: '32-feet extension ladder', cols: cols, rows: 1, image: "https://images-na.ssl-images-amazon.com/images/I/31VJHN3H1JL._AC_.jpg", desc: "Can reach your roof" },
+        {
+          title: 'Pressure Washer',
+          cols: cols,
+          rows: 1,
+          image:
+            'https://images-na.ssl-images-amazon.com/images/I/91Or5s3f24L._AC_SL1500_.jpg',
+          desc: 'Pressure wash your deck',
+          price: '$30/week',
+        },
+        {
+          title: 'Edger and Trimmer',
+          cols: cols,
+          rows: 1,
+          image:
+            'https://images-na.ssl-images-amazon.com/images/I/512jaAufjAL._AC_SL1000_.jpg',
+          desc: 'Trim your grass edge',
+          price: '$20/week',
+        },
+        {
+          title: 'Nailer Gun',
+          cols: cols,
+          rows: 1,
+          image:
+            'https://images-na.ssl-images-amazon.com/images/I/51gokfO5USL._AC_SL1000_.jpg',
+          desc: 'Easy to use for small project',
+          price: '$20/week',
+        },
+        {
+          title: 'Titan Impact 440 paint sprayer',
+          cols: cols,
+          rows: 1,
+          image:
+            'https://titantool.1scri.be/media/catalog/product/cache/b18383ece3cd1a2211c659793c25b122/0/3/03__impact_440_sk.png',
+          desc: 'Professional paint spayer,',
+          price: '$50/week',
+        },
 
-        { title: 'Service', cols: cols, rows: 1, image: "assets/ww-logo.svg", desc: "Weekend Warrior service" },
+        {
+          title: 'Miter Saw',
+          cols: cols,
+          rows: 1,
+          image:
+            'https://images-na.ssl-images-amazon.com/images/I/71TPrh4okNL._AC_SL1500_.jpg',
+          desc: 'For small woodworking projects, with a stand',
+          price: '$30/week',
+        },
+        {
+          title: 'Table Saw',
+          cols: cols,
+          rows: 1,
+          image:
+            'https://images.thdstatic.com/productImages/3f75b59d-b153-45fd-ad06-052296e477f1/svn/dewalt-portable-table-saws-dw745-64_600.jpg',
+          desc: 'For small woodworking projects, with a stand',
+          price: '$30/week',
+        },
+        {
+          title: 'Drill Set',
+          cols: cols,
+          rows: 1,
+          image:
+            'https://images-na.ssl-images-amazon.com/images/I/91WzLZ1qASL._AC_SL1500_.jpg',
+          desc: 'Any project would be needed',
+          price: '$10/week',
+        },
+        {
+          title: '22-foot telescoping ladder',
+          cols: cols,
+          rows: 1,
+          image:
+            'https://images-na.ssl-images-amazon.com/images/I/51M9J2MlE9S._AC_SL1000_.jpg',
+          desc: 'A versitle ladder for anything',
+          price: '$20/week',
+        },
+        {
+          title: '32-feet extension ladder',
+          cols: cols,
+          rows: 1,
+          image:
+            'https://images-na.ssl-images-amazon.com/images/I/31VJHN3H1JL._AC_.jpg',
+          desc: 'Can reach your roof',
+          price: '$30/week',
+        },
       ];
     })
   );
