1377a688a476a35dd0219135eb7dd85e4424c0a7
Chao Yu <chaoyu.bupter@gmail.com>
2021-04-10 21:22:07 -0700

    add telegram, add gtag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
+declare let gtag: Function;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'weekendwarrior';
@@ -11,4 +13,14 @@ export class AppComponent {
   raisedElevation = 8;
 
   name = 'Angular';
+
+  constructor(public router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        gtag('config', 'G-R246TZGZM5', {
+          'page_path': event.urlAfterRedirects,
+        });
+      }
+    });
+  }
 }
